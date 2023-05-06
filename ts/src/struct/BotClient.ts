0f795b4300687270e2f189ed20719965ce5a5d10
Kitekat <reshkirill00@gmail.com>
2023-05-06 12:36:29 +0300

    create loadCommands

--- ts/src/struct/BotClient.ts
+++ ts/src/struct/BotClient.ts
@@ -13,7 +13,29 @@ export class BotClient extends Client {
     this.commands = new Collection<string, Command>();
   }
 
-  private async loadCommands(){}
+  private async loadCommands(...dirs: string[]) {
+    for (const dir of dirs) {
+      const files = await readdir(join(__dirname, dir));
+
+      for (const file of files) {
+        const stat = await lstat(join(__dirname, dir, file));
+        if (stat.isDirectory()) await this.loadCommands(join(dir, file))
+        else {
+          if (file.endsWith('.js')) {
+            try {
+              const module: Command = new (
+                  await import(join(__dirname, dir, file))
+              ).default(this);
+
+              this.commands.set(module.name, module);
+            } catch (e) {
+              console.error(`Ошибка при загрузке команды из файла ${file}: ${e}`);
+            }
+          }
+        }
+      }
+    }
+  }
 
   protected async registerCommands(...dirs: string[]) {
     for (const dir of dirs) {
@@ -21,7 +43,7 @@ export class BotClient extends Client {
 
       for (const file of files) {
         const stat = await lstat(join(__dirname, dir, file));
-        if (stat.isDirectory()) await this.registerCommands(join(dir, file));
+        if (stat.isDirectory()) await this.registerCommands(join(dir, file))
         else {
           if (file.endsWith('.js')) {
             try {
