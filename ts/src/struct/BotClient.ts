feb31836aa6eba1a431d486db58b0676d7e5eb1e
Akavi <gabdushev.i@yandex.ru>
2023-05-06 14:04:57 +0500

    (feat) need to finish registerCommands

--- ts/src/struct/BotClient.ts
+++ ts/src/struct/BotClient.ts
@@ -1,6 +1,8 @@
 import { Client, Collection } from 'discord.js';
 import { ClientOptions } from 'discord.js';
 import { Command } from './Command';
+import { readdir, lstat } from 'fs/promises';
+import { join } from 'path';
 
 export class BotClient extends Client {
   commands: Collection<string, Command>;
@@ -9,6 +11,28 @@ export class BotClient extends Client {
     super(options);
 
     this.commands = new Collection<string, Command>();
+  }
+
+  private async loadCommands(){}
+
+  protected async registerCommands(...dirs: string[]) {
+    for (const dir of dirs) {
+      const files = await readdir(join(__dirname, dir));
+
+      for (const file of files) {
+        const stat = await lstat(join(__dirname, dir, file));
+        if (stat.isDirectory()) await this.registerCommands(join(dir, file));
+        else {
+          if (file.endsWith('.js')) {
+            try {
+              const module: Command = new (
+                await import(join(__dirname, dir, file))
+              ).default(this);
+            } catch (e) {}
+          }
+        }
+      }
+    }
   }
 
   public async start(token: string) {
