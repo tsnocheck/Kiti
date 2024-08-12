3a4aaf90cd5f9d198d2dd35a72a5f049f49c3821
Kirill Reshetnikov <99495151+urumadev@users.noreply.github.com>
2024-08-12 21:58:03 +0300

    add error metrics

--- src/lib/prometheus/client.ts
+++ src/lib/prometheus/client.ts
@@ -9,11 +9,17 @@ class PrometheusClient {
   
   private guildCountGauge: Gauge<string>;
   private userCountGauge: Gauge<string>;
-  private formCountGauge: Gauge<string>
+  private formCountGauge: Gauge<string>;
+  
   private commandCountGauge: Counter<string>;
   private buttonCountGauge: Counter<string>;
   private modalCountGauge: Counter<string>;
   private selectMenuCountGauge: Counter<string>;
+  
+  private commandErrorCountGauge: Counter<string>;
+  private buttonErrorCountGauge: Counter<string>;
+  private modalErrorCountGauge: Counter<string>;
+  private selectMenuErrorCountGauge: Counter<string>;
   
   constructor() {
     this.registry = new promClient.Registry();
@@ -72,6 +78,30 @@ class PrometheusClient {
       help: 'The total number of select menu interactions',
       registers: [this.registry],
     });
+    
+    this.commandErrorCountGauge = new Counter({
+      name: 'error_command_interactions',
+      help: 'The total number of command interactions',
+      registers: [this.registry],
+    });
+    
+    this.buttonErrorCountGauge = new Counter({
+      name: 'error_button_interactions',
+      help: 'The total number of button interactions',
+      registers: [this.registry],
+    });
+    
+    this.modalErrorCountGauge = new Counter({
+      name: 'error_modal_interactions',
+      help: 'The total number of modal interactions',
+      registers: [this.registry],
+    });
+    
+    this.selectMenuErrorCountGauge = new Counter({
+      name: 'error_select_menu_interactions',
+      help: 'The total number of select menu interactions',
+      registers: [this.registry],
+    });
   }
 
   public async pushGuildCount(guildCount: number) {
@@ -108,6 +138,29 @@ class PrometheusClient {
             this.selectMenuCountGauge.inc();
             await this.pushMetrics();
           }
+        break
+    }
+  }
+  
+  public async incrementErrorInteraction(interaction: BaseInteraction<CacheType>) {
+    switch (interaction.type){
+      case InteractionType.ModalSubmit:
+        this.modalErrorCountGauge.inc();
+        await this.pushMetrics();
+        break
+      case InteractionType.ApplicationCommand:
+        this.commandErrorCountGauge.inc();
+        await this.pushMetrics();
+        break
+      case InteractionType.MessageComponent:
+        if(interaction.isButton()){
+          this.buttonErrorCountGauge.inc();
+          await this.pushMetrics();
+        }
+        if(interaction.isStringSelectMenu()){
+          this.selectMenuErrorCountGauge.inc();
+          await this.pushMetrics();
+        }
         break
     }
   }
