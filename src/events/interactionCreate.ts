dc8633e61e7aecb77798a243abfdb9eb34f35ee2
Kirill Reshetnikov <99495151+urumadev@users.noreply.github.com>
2024-08-12 21:31:16 +0300

    send metrics on events

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,10 +1,12 @@
 import {IEvent} from "../lib/discord/Event";
 import {BaseInteraction} from "discord.js";
 import {BotClient} from "../lib/discord/Client";
+import { PrometheusClient } from '../lib/prometheus/client'
 
 export default class InteractionCreateEvent implements IEvent {
   name = "interactionCreate";
   run = async (client: BotClient, interaction: BaseInteraction) => {
+    await client.metrics.incrementInteraction(interaction)
     if (interaction.isCommand()) {
       const command = client.commands.get(interaction.commandName);
 
