f0e6a072f2107f3013da41d1f45f779663513e5b
Kirill Reshetnikov <99495151+urumadev@users.noreply.github.com>
2024-08-12 21:58:36 +0300

    add error metrics and logger.error interactions

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,22 +1,38 @@
+import { InteractionType } from 'discord-api-types/v10'
 import {IEvent} from "../lib/discord/Event";
 import {BaseInteraction} from "discord.js";
 import {BotClient} from "../lib/discord/Client";
 import { PrometheusClient } from '../lib/prometheus/client'
+import { logger } from '../services/logger'
 
 export default class InteractionCreateEvent implements IEvent {
   name = "interactionCreate";
   run = async (client: BotClient, interaction: BaseInteraction) => {
-    await client.metrics.incrementInteraction(interaction)
-    if (interaction.isCommand()) {
-      const command = client.commands.get(interaction.commandName);
-
-      if (command) {
-        command.run({interaction, client});
+    try{
+      await client.metrics.incrementInteraction(interaction)
+      if (interaction.isCommand()) {
+        const command = client.commands.get(interaction.commandName);
+        
+        if (command) {
+          command.run({interaction, client});
+        }
+      } else if (interaction.isMessageComponent()) {
+        const feature = client.features.get(interaction.customId);
+        if (feature) {
+          feature.run({interaction, client});
+        }
       }
-    } else if (interaction.isMessageComponent()) {
-      const feature = client.features.get(interaction.customId);
-      if (feature) {
-        feature.run({interaction, client});
+    }catch(error){
+      await client.metrics.incrementErrorInteraction(interaction)
+      if(interaction.type === InteractionType.MessageComponent){
+        if(interaction.isButton()){
+          logger.error(`Type Button - ${error}`)
+        }
+        if(interaction.isStringSelectMenu()){
+          logger.error(`Type StringSelectMenu - ${error}`)
+        }
+      }else{
+        logger.error(`Type ${interaction.type} - ${error}`)
       }
     }
   }
