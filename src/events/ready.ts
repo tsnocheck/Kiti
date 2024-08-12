725693ab98fab738da0435b562e2442c157e982e
Kirill Reshetnikov <99495151+urumadev@users.noreply.github.com>
2024-08-12 22:07:35 +0300

    add metrics for stats userCount, formCount and guildCount

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,19 @@
 import {IEvent} from "../lib/discord/Event";
+import { getModelForClass } from '@typegoose/typegoose'
+import { Form } from '../lib/schemas/Form'
+import { User } from '../lib/schemas/User'
 
 export default class ReadyEvent implements IEvent {
 
   name = 'ready'
 
-  run(client: any) {
-    console.log(`Logged in as ${client.user.tag}!`);
+  async run(client: any) {
+    const UserModel = getModelForClass(User);
+    const FormModel = getModelForClass(Form);
+    let guildCount = await client.guilds.fetch()
+    
+    await client.metrics.pushGuildCount(guildCount.size)
+    await client.metrics.pushUserCount(UserModel.length)
+    await client.metrics.pushFormCount(FormModel.length)
   }
 }
