08932da7fcce20517021cecd07ea7b22210aa80a
Akavi <gabdushev.i@yandex.ru>
2024-08-12 13:06:03 +0500

    (feat) move logs to json, use mode env

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,30 +1,26 @@
 import winston from 'winston';
-import { format } from 'date-fns';
-
-const customTimestampFormat = winston.format((info) => {
-	info.timestamp = format(new Date(), 'HH:mm');
-	return info;
-})();
 
 const logger = winston.createLogger({
-	level: 'info',
-	format: winston.format.combine(
-		customTimestampFormat,
-		winston.format.printf(({ timestamp, level, message }) => {
-			return `${timestamp} [${level}]: ${message}`;
-		})
-	),
-	transports: [
-		new winston.transports.Console({
-			format: winston.format.combine(
-				winston.format.colorize(),
-				winston.format.printf(({ timestamp, level, message }) => {
-					return `${timestamp} [${level}]: ${message}`;
-				})
-			)
-		}),
-		new winston.transports.File({ filename: 'combined.log' })
-	],
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: 'bot',
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.printf(({timestamp, level, message}) => {
+          return `${timestamp} [${level}]: ${message}`;
+        })
+      )
+    }),
+    new winston.transports.File({filename: 'combined.log'}),
+    new winston.transports.File({filename: 'error.log', level: 'error'})
+  ],
 });
 
-export { logger };
+if (process.env.MODE !== "prod") {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+  }));
+}
+
+export {logger};
