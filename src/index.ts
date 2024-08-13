8a8dd042e841e9967227b8288758e8f814f239a8
Kirill Reshetnikov <99495151+urumadev@users.noreply.github.com>
2024-08-13 18:45:25 +0300

    identified redis client

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import { createClient, type RedisClientType } from 'redis'
 import {BotClient} from "./lib/discord/Client";
 import {config} from "dotenv";
 config()
 
-const client = new BotClient()
+const redis: RedisClientType = createClient({
+  url: 'redis://root:admin@redis:6379'
+})
+
+const client = new BotClient(redis)
 
 client.build(process.env.TOKEN!)
