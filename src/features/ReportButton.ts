9d4c5ff9b1ae41cadf25bf41ddc89cacbf7332c8
Akavi <gabdushev.i@yandex.ru>
2024-08-15 22:51:17 +0500

    (feat) report ui. (todo) need to make reportusecase for moderation to ban users or give shadowban

--- src/features/ReportButton.ts
+++ src/features/ReportButton.ts
@@ -1,10 +1,42 @@
-import { IFeature } from "../lib/discord/Feature";
-import { ButtonInteraction } from "discord.js";
+import {IFeature} from "../lib/discord/Feature";
+import {ActionRowBuilder, ButtonInteraction, StringSelectMenuBuilder} from "discord.js";
 
 export default class ButtonFeature implements IFeature<ButtonInteraction> {
   name = "ReportButton";
-  
-  async run({ interaction }: { interaction: ButtonInteraction }): Promise<any> {
-    await interaction.reply("Double Pong!");
+
+  async run({interaction}: { interaction: ButtonInteraction }): Promise<any> {
+    const userId = interaction.customId.split('_')[1];
+    const row = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+      new StringSelectMenuBuilder()
+        .setCustomId(`ReportMenu_${userId}`)
+        .setPlaceholder('Выберите причину жалобы')
+        .setOptions([
+          {
+            label: 'Материал для взрослых',
+            emoji: '🔞',
+            value: 'nsfw_content'
+          },
+          {
+            label: 'Продажа товаров и услуг.',
+            emoji: '💰',
+            value: 'commerce',
+          },
+          {
+            label: 'Оскорбление или вызывающие высказывания',
+            emoji: '📄',
+            value: 'language',
+          },
+          {
+            label: 'Другое',
+            emoji: '📜',
+            value: 'other',
+          },
+        ])
+    );
+
+    await interaction.reply({
+      content: 'Выберите причину жалобы на анкету',
+      components: [row]
+    });
   }
 }
