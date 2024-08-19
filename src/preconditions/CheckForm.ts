4da3c8d5d79d8f1fc5714ece811dfdf467fb30df
Kirill Reshetnikov <99495151+urumadev@users.noreply.github.com>
2024-08-19 21:28:04 +0300

    я не ебу что это

--- src/preconditions/CheckForm.ts
+++ src/preconditions/CheckForm.ts
@@ -14,7 +14,7 @@ export default class CheckForm implements IPrecondition {
 
   async run({interaction, client}: { interaction: BaseInteraction, client: BotClient }) {
     let user = await client.userUsecase?.findByUserId(interaction.user.id);
-
+    
     if (user == null) {
       let button = new ActionRowBuilder<ButtonBuilder>()
         .addComponents(
@@ -28,9 +28,10 @@ export default class CheckForm implements IPrecondition {
         .setTitle('Анкета')
         .setColor(0x2b2d31)
         .setDescription('**Создавая анкету вы автоматически подписываете [Пользовательское соглашение](https://docs.google.com/document/d/1pnNdL2rUVqurlKscDZNy-ABdPw-ZfDdQXOlo6kwnxvM/edit#heading=h.czgykm6tiowt) и [Политику конфиденциальности](https://docs.google.com/document/d/1IVsThKB01CVMeXI551f5LfdV-ICi0-1_ydPyNukVB3c/edit#heading=h.w7quzb7fkv1l)**');
-      await (interaction as RepliableInteraction).reply({embeds: [embed], components: [button], fetchReply: true});
+      await (interaction as RepliableInteraction).reply({embeds: [embed], components: [button]});
       return false;
+    }else{
+      return true;
     }
-    return true;
   }
 }
