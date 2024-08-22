8e19cea97d57e8dfa8e3144c7f88e8592449499a
Akavi <gabdushev.i@yandex.ru>
2024-08-22 12:37:13 +0500

    (chore) make logger more readable in docker container

--- src/lib/services/logger.ts
+++ src/lib/services/logger.ts
@@ -5,8 +5,8 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   defaultMeta: 'bot',
   transports: [
-    new winston.transports.File({filename: 'combined.log'}),
-    new winston.transports.File({filename: 'error.log', level: 'error'})
+    new winston.transports.File({filename: 'logs/combined.log'}),
+    new winston.transports.File({filename: 'logs/error.log', level: 'error'})
   ],
 });
 
