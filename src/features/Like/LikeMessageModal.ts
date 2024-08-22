779bef519314d7881eb2e28fa98d4f1c2ce709f6
Kirill Reshetnikov <99495151+urumadev@users.noreply.github.com>
2024-08-23 00:04:38 +0300

    add like profile

---
+++ src/features/Like/LikeMessageModal.ts
@@ -0,0 +1,80 @@
+import {IFeature} from "../../lib/discord/Feature";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonInteraction,
+  ButtonStyle,
+  EmbedBuilder,
+  ModalBuilder, type ModalSubmitInteraction, TextInputBuilder, TextInputStyle,
+  type User
+} from 'discord.js'
+import {BotClient} from "../../lib/discord/Client";
+
+export default class LikeMessageModal implements IFeature<ModalSubmitInteraction> {
+  name = "LikeMessageModal";
+
+  async run({interaction, client}: { interaction: ModalSubmitInteraction, client: BotClient }): Promise<any> {
+    const message = interaction.fields.getTextInputValue('message');
+    await interaction.deferUpdate()
+    
+    let usercases = client.userUsecase;
+    if (interaction.deferred || interaction.replied) return;
+    const userForm = await client.userUsecase.findByUserId(interaction.user.id)
+    
+    if(!userForm?.shadowBanned){
+      await usercases.likeMessage(interaction.user.id, interaction.customId.split('_')[1], message).catch(() => {});
+      
+      const member: User = await client.users.fetch(interaction.customId.split('_')[1]) as User;
+      
+      let dmMessage = new EmbedBuilder()
+        .setTitle('Анкета')
+        .setDescription(`**Ты понравился 1 человеку. Хочешь посмотреть кому понравился? Тогда используй команду \`/like\`**`)
+        .setColor('#bbffd3')
+      
+      await member.send({embeds:[dmMessage]})
+    }
+    
+    let form = await usercases.getRandomForm(interaction.user.id);
+    let err = new EmbedBuilder()
+      .setTitle('Анкеты')
+      .setDescription('К сожалению активные анкеты кончились. Попробуйте позже.')
+      .setColor('#bbffd3')
+    
+    if(!form){
+      await interaction.editReply({embeds:[err], components:[]})
+      return
+    }
+    
+    let embed = new EmbedBuilder()
+      .setTitle('Анкета')
+      .setDescription(`
+         ${form?.name}, ${form?.age}, ${form?.city}
+         ${form?.status}
+      `)
+      .setColor('#bbffd3')
+      .setImage(form?.photo || null);
+    
+    const buttons = new ActionRowBuilder<ButtonBuilder>()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId(`LikeButton_${form?.userId}`)
+          .setEmoji('<:likeIcon:1273558975966744620>')
+          .setStyle(ButtonStyle.Secondary),
+        new ButtonBuilder()
+          .setCustomId(`LikeMessageButton_${form?.userId}`)
+          .setEmoji('<:likeMessageIcon:1273558952235241557>')
+          .setStyle(ButtonStyle.Secondary)
+          .setDisabled(true),
+        new ButtonBuilder()
+          .setCustomId(`DisLikeButton_${form?.userId}`)
+          .setEmoji('<:dislikeIcon:1273559004014055497>')
+          .setStyle(ButtonStyle.Secondary),
+        new ButtonBuilder()
+          .setCustomId(`ReportButton_${form?.userId}`)
+          .setEmoji('<:ticketIcon:1273559224940494858>')
+          .setStyle(ButtonStyle.Secondary),
+      );
+    await interaction.editReply({embeds: [embed], components: [buttons]}).catch(() => {  });
+    await interaction.followUp({content: 'Вы успешно лайкнули анкету', ephemeral: true}).catch(() => {})
+  }
+}
---
+++ src/lib/schemas/Messages.ts
@@ -0,0 +1,13 @@
+import {prop, Ref} from "@typegoose/typegoose";
+import {User} from "./User";
+
+export class Messages {
+  @prop()
+  message!: string;
+  
+  @prop({ref: () => User})
+  from?: Ref<User>;
+
+  @prop({ref: () => User})
+  to?: Ref<User>;
+}
